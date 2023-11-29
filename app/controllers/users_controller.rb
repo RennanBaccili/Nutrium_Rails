01326da9673d397d34dbf41e3091149b10006d68
Rennan Bacili <rennanbaccili@gmail.com>
2023-11-29 19:35:51 -0300

    User estilizado

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to @user, notice: "User was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @user.save ? (redirect_to @user) : (render :new)
   end
 
   # PATCH/PUT /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :full_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 } # Ajuste o comprimento conforme necessário
+  validates :gender, :country, :date_of_birth, :phone, :status, :profession, presence: true
 end
+
+
